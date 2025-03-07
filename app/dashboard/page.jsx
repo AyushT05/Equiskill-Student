9f68dfdcf406c4a68a84e255c26c18a4f9113760
AyushT05 <ayushthanthri2005@gmail.com>
2025-03-07 20:48:18 +0530

    login redirect fix 2

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import WelcomeBanner from './_components/WelcomeBanner'
 import CourseList from './_components/CourseList'
-import { auth } from "@clerk/nextjs/server";
+
 
 
 async function Dashboard() {
   
-  const { userId } = await auth();
-  if (!userId) {
-    return redirect("/sign-in");
-  }
+  
   return (
     <div>
      
