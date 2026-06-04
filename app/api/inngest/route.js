8d312188212b99a3c1edfcf760c792b47fe73fea
AyushT05 <ayushthanthri2005@gmail.com>
2026-06-05 01:07:44 +0530

    Debugging issue 1

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,16 +1,16 @@
+console.log("SIGNING KEY:", process.env.INNGEST_SIGNING_KEY?.slice(0, 20));
+
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { CreateNewUser, GenerateNotes, GenerateStudyTypeContent, helloWorld } from "@/inngest/functions";
-//export const runtime = 'edge';
 
-// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   streaming: 'allow',
   functions: [
     helloWorld,
-   CreateNewUser,
-   GenerateNotes,
-   GenerateStudyTypeContent,
+    CreateNewUser,
+    GenerateNotes,
+    GenerateStudyTypeContent,
   ],
 });
